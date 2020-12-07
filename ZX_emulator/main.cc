2be9e189966771ea5323a6efbd143ada13c941de
sereg <sereg@DESKTOP-68U5RL6>
2020-12-08 00:57:41 +0500

    zadacha

---
+++ ZX_emulator/main.cc
@@ -0,0 +1,30 @@
+#include <vector>
+#include <cstdint>
+#include <random>
+#include <iostream>
+#include <fstream>
+
+#include <SDL2/SDL.h>
+
+#include "Window.h"
+
+int main(int, char **)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	try {
+			Window win;
+
+			win.main();
+		} catch (const std::exception & e) {
+			std::cerr << e.what() << std::endl;
+			return 1;
+		} catch (...) {
+			std::cerr << "An unknown exception has occured!" << std::endl;
+			return 2;
+		}
+	return 0;
+}
+
+
+
